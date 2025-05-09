6bbe461fb3b18fb80e43f7e1b1c03563c8940883
zainuriss <faiz.zainuri123@gmail.com>
2025-05-09 21:50:06 +0700

    first init

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,86 @@
 import Image from "next/image";
+import MyImage from "../public/test.png";
+import { EB_Garamond, Lexend } from "next/font/google";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(fab, fas);
+const ebGaramond = EB_Garamond({ subsets: ["latin"] });
+const lexend = Lexend({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <div
+        className={` ${lexend.className} flex justify-center items-center min-h-screen bg-neutral-900 w-full`}
+      >
+        <div className="flex flex-nowrap flex-row justify-around items-center h-auto w-full">
+          <div className="w-full flex justify-center flex-col ml-10">
+            <h1 className={`text-[15em]/50 text-neutral-800/30 font-bold`}>
+              PORT
+            </h1>
+            <h1 className={`text-[15em]/50 text-neutral-800/30 font-bold`}>
+              FOLIO
+            </h1>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="absolute w-full flex justify-around">
+            <div className="w-full flex justify-around items-center">
+              <div className="flex flex-col items-center gap-4 transform transition ease-in-out duration-500">
+                <a
+                  href="https://wa.me/6289673799600"
+                  className="text-[#82c0cc] hover:text-neutral-900 hover:bg-[#82c0cc] p-4 rounded-full transition duration-500 ease-in-out transform hover:scale-110 w-14"
+                >
+                  <FontAwesomeIcon icon={["fab", "whatsapp"]} />
+                </a>
+                <a
+                  href="https://www.instagram.com/zainuriss_/"
+                  className="text-[#82c0cc] hover:text-neutral-900 hover:bg-[#82c0cc] p-4 rounded-full transition duration-500 ease-in-out transform hover:scale-110 w-14"
+                >
+                  <FontAwesomeIcon icon={["fab", "instagram"]} />
+                </a>
+                <a
+                  href="mailto:zzainuri.me@gmail.com"
+                  className="text-[#82c0cc] hover:text-neutral-900 hover:bg-[#82c0cc] p-4 rounded-full transition duration-500 ease-in-out transform hover:scale-110 w-14"
+                >
+                  <FontAwesomeIcon icon={["fas", "envelope"]} />
+                </a>
+              </div>
+              <div className="flex">
+                <h1
+                  className={`text-8xl flex flex-col text-left text-[#82c0cc] font-bold`}
+                >
+                  Heyyo,
+                </h1>
+              </div>
+              <div className={`gap-4 flex-col flex text-[#82c0cc]`}>
+                <a
+                  href="#about"
+                  className="relative inline-block px-3 py-1 text-neutral-700 font-semibold transition-transform duration-300 hover:scale-105 hover:text-[#82c0cc] after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-full after:h-[2px] after:bg-gradient-to-r after:from-cyan-400 after:to-green-300 after:scale-x-0 after:origin-right after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-left"
+                >
+                  About me
+                </a>
+
+                <a
+                  href="#skills"
+                  className="relative inline-block px-3 py-1 text-neutral-700 font-semibold transition-transform duration-300 hover:scale-105 hover:text-[#82c0cc] after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-full after:h-[2px] after:bg-gradient-to-r after:from-cyan-400 after:to-green-300 after:scale-x-0 after:origin-right after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-left"
+                >
+                  Skills
+                </a>
+
+                <a
+                  href="#projects"
+                  className="relative inline-block px-3 py-1 text-neutral-700 font-semibold transition-transform duration-300 hover:scale-105 hover:text-[#82c0cc] after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-full after:h-[2px] after:bg-gradient-to-r after:from-cyan-400 after:to-green-300 after:scale-x-0 after:origin-right after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-left"
+                >
+                  Projects
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.rawpixel.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
